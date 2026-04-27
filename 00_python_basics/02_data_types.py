aa1494fde472628fd4153116dfe357617502ac97
Gift10477 <giftgithaka@gmail.com>
2026-04-27 12:35:51 +0300

    new line

---
+++ 00_python_basics/02_data_types.py
@@ -0,0 +1,6 @@
+name = "John"
+num = 147
+flag = True
+balance = 100.50
+print(type(name)) #type gets the variable type and prints it out
+print(id(name)) #id get the address of the variable and prints the address memory
